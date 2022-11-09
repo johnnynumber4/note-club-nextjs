69b402988fd9c63d5defe3b2e76597cf0c154a4b
Johnny Number Four <jyoungiv@gmail.com>
2022-11-09 11:08:15 -0500

    changing input text

--- page-components/Feed/Poster.jsx
+++ page-components/Feed/Poster.jsx
@@ -48,8 +48,8 @@ const PosterInner = ({ user }) => {
         <Input
           ref={contentRef}
           className={styles.input}
-          placeholder={`What's on your mind, ${user.name}?`}
-          ariaLabel={`What's on your mind, ${user.name}?`}
+          placeholder={`What should we listen to, ${user.name}?`}
+          ariaLabel={`What should we listen to, ${user.name}?`}
         />
         <Button type="success" loading={isLoading}>
           Post
@@ -66,7 +66,7 @@ const Poster = () => {
   return (
     <Wrapper>
       <div className={styles.root}>
-        <h3 className={styles.heading}>Share your thoughts</h3>
+        <h3 className={styles.heading}>Post your album!</h3>
         {loading ? (
           <LoadingDots>Loading</LoadingDots>
         ) : data?.user ? (

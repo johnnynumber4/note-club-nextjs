2627002f61d103fbee0411d375b0088cfc0d6bf6
Johnny Number Four <jyoungiv@gmail.com>
2022-11-09 17:03:02 -0500

    changed meta tag info

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -17,14 +17,14 @@ const Layout = ({ children }) => {
           name="description"
           content="We like to listen to music and share our thoughts on the albums we select.  No matter how bad or good, they will live forever in this repo."
         />
-        <meta property="og:title" content="Next.js + MongoDB App" />
+        <meta property="og:title" content="Note Club: Club of Notes" />
         <meta
           property="og:description"
           content="We like to listen to music and share our thoughts on the albums we select.  No matter how bad or good, they will live forever in this repo."
         />
         <meta
           property="og:image"
-          content="https://repository-images.githubusercontent.com/201392697/5d392300-eef3-11e9-8e20-53310193fbfd"
+          content="https://cdns-images.dzcdn.net/images/artist/238f5524a401dfdd5cac685f0f7989bd/500x500.jpg"
         />
       </Head>
       <Nav />

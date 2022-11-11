0c54b12a13b0831d3f3588c0f5454b0e6bc377d8
Johnny Young <jyoungiv@gmail.com>
2022-11-11 12:49:42 -0500

    manifest changes

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -20,11 +20,11 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <meta charset="utf-8" />
-          <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
-          <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-          <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/500x500.jpg" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
           <Main />

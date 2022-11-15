7743098ac054e378b6cc3549e03d021ced7466de
Johnny Young <jyoungiv@gmail.com>
2022-11-14 22:09:12 -0500

    created listenlinks

---
+++ components/ListenLinks/ListenLinks.jsx
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+import styles from './Description.module.css';
+import Image from 'next/image';
+
+const ListenLinks = ({ post, className }) => {
+  return (
+    <div className={clsx(styles.root, className)}>
+      <div className={styles.wrap}>
+        <div>
+          <p>
+            <a
+              href={`https://music.youtube.com/playlist?list=${post.yt}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ zIndex: 99 }}
+            >
+              <Image
+                src="/icons/Youtube_Music_icon.png"
+                alt="YouTube Music Search"
+                width="80"
+                height="80"
+              />
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListenLinks;

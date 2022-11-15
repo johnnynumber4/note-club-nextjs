690157e118509ad9167d0a7a6fd6b48593af1c27
Johnny Young <jyoungiv@gmail.com>
2022-11-14 22:02:12 -0500

    added listen now section

--- page-components/UserPost/index.jsx
+++ page-components/UserPost/index.jsx
@@ -4,12 +4,33 @@ import { Post as PostItem } from '@/components/Post';
 import Commenter from './Commenter';
 import CommentList from './CommentList';
 import styles from './UserPost.module.css';
+import Image from 'next/image';
 
 export const UserPost = ({ post }) => {
   return (
     <Wrapper>
       <Spacer size={2} axis="vertical" />
       <PostItem post={post} />
+      <h3 className={styles.subtitle}>Listen Now:</h3>
+      {post.yt && (
+        <div>
+          <p>
+            <a
+              href={`https://music.youtube.com/playlist?list=${post.yt}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ zIndex: 99 }}
+            >
+              <Image
+                src="/icons/Youtube_Music_icon.png"
+                alt="YouTube Music Search"
+                width="80"
+                height="80"
+              />
+            </a>
+          </p>
+        </div>
+      )}
       <h3 className={styles.subtitle}>About the Album:</h3>
       <Description post={post} />
       <h3 className={styles.subtitle}>Comments</h3>

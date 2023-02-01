b3a422f431c09cd02a4db789beb4291adfbcfaea
Johnny Number Four <jyoungiv@gmail.com>
2023-02-01 14:05:42 -0500

    lint fix

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,9 +1,5 @@
 import { ValidateProps } from '@/api-lib/constants';
-import { 
-  findPosts,
-  insertPost,
-  // findPostById
-} from '@/api-lib/db';
+import { findPosts, insertPost } from '@/api-lib/db';
 import { auths, validateBody } from '@/api-lib/middlewares';
 import { getMongoDb } from '@/api-lib/mongodb';
 import { ncOpts } from '@/api-lib/nc';

4f5c2efbbefadd8d62fbe13d71890c364dff775a
Johnny Young <jyoungiv@gmail.com>
2023-03-19 13:27:31 -0400

    fixes everywhere!

--- page-components/Feed/PostList.jsx
+++ page-components/Feed/PostList.jsx
@@ -1,9 +1,7 @@
-import { Button } from '@/components/Button';
-import { Container, Spacer } from '@/components/Layout';
+import { Spacer } from '@/components/Layout';
 import Wrapper from '@/components/Layout/Wrapper';
 import { Post } from '@/components/Post';
 // import { Search } from '@/components/Search';
-import { Text } from '@/components/Text';
 import { usePostPages } from '@/lib/post';
 import Link from 'next/link';
 import styles from './PostList.module.css';

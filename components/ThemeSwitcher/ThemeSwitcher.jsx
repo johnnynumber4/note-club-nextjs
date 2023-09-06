b95dbb279fb8eec3a5c8d7a2354a16c5cd53ddca
Johnny Number Four <jyoungiv@gmail.com>
2023-09-06 15:00:52 +0000

    Update 4 files

--- components/ThemeSwitcher/ThemeSwitcher.jsx
+++ components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,3 +1,30 @@
+// import { useTheme } from 'next-themes';
+// import { useCallback } from 'react';
+// import styles from './ThemeSwitcher.module.css';
+
+// const ThemeSwitcher = () => {
+//   const { theme, setTheme } = useTheme();
+//   const onChange = useCallback(
+//     (e) => {
+//       setTheme(e.target.checked ? 'light' : 'dark');
+//     },
+//     [setTheme]
+//   );
+//   return (
+//     // <select value={theme} onChange={onChange} className={styles.select}>
+//     //   <option value="system">System</option>
+//     //   <option value="dark">Dark</option>
+//     //   <option value="light">Light</option>
+//     // </select>
+//     <div className={styles.toggle}>
+//       <input value="system" onChange={onChange} type="checkbox" />
+//       <label></label>
+//     </div>
+//   );
+// };
+
+// export default ThemeSwitcher;
+
 import { useTheme } from 'next-themes';
 import { useCallback } from 'react';
 import styles from './ThemeSwitcher.module.css';
